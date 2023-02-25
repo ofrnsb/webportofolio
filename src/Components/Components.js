233730733ccff005bd5b942fcd2f39cb7ad9fbd7
b <ofbutar@gmail.com>
2023-02-26 01:42:31 +0700

    add main page

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -1,11 +1,11 @@
-const { default: styled } = require('styled-components');
+const { default: styled, keyframes } = require('styled-components');
 
 export const Scontainer = styled.div`
   background-color: #ebebeb;
 
   min-height: 100vh;
 
-  padding: 4rem clamp(1rem, 5vw, 5rem) clamp(5rem, 5vw, 5rem)
+  padding: clamp(1rem, 5vw, 5rem) clamp(1rem, 5vw, 5rem) clamp(4rem, 2vw, 5rem)
     clamp(1rem, 5vw, 5rem);
 
   position: relative;
@@ -16,14 +16,44 @@ export const Scontainer = styled.div`
   gap: clamp(1.5rem, 2.5vw, 2.5rem);
 `;
 
+const Snav_Animation = keyframes`
+  0% {
+    width: 0;
+  }
+
+  50% {
+    width: 100%;
+  }
+
+  100%{
+    box-shadow: 0 0.5rem 0.75rem 0.25rem #dedede;
+  }
+`;
+
 export const Snav = styled.main`
-  padding: 0.5rem 1rem;
+  padding: 0.5rem 2rem;
+  position: relative;
+
   border-radius: 3rem;
+
   display: flex;
   align-items: center;
-  background-color: #faf8ff;
+  background-color: #f0f1f1;
 
   font-weight: 500;
   color: black;
   font-size: clamp(1rem, 4.75vw, 3rem);
+
+  animation: ${Snav_Animation} 1.5s ease-in forwards;
+
+  div {
+    display: flex;
+    align-items: center;
+    color: ${(props) => (props.isAnimated ? '#2d2d2d' : 'transparent')};
+    transition: color 0.5s ease-in;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
 `;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,32 @@
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { Snav } from './Components';
 
 const Navbar = () => {
+  const [isAnimated, setisAnimated] = useState(false);
+
+  useEffect(() => {
+    let isTrue = true;
+
+    setTimeout(() => {
+      isTrue ? setisAnimated(true) : null;
+    }, 750);
+
+    return () => {
+      let isTrue = false;
+    };
+  }, []);
+
   return (
-    <>
-      <Snav>
-        Ob
-        <sup>
-          <sup>
-            <sup>®</sup>
-          </sup>
-        </sup>
-        — 23
-      </Snav>
-    </>
+    <Snav isAnimated={isAnimated}>
+      <Link href="/">
+        <div>
+          Ob
+          <sup>®</sup>
+          {isAnimated && '— 23'}
+        </div>
+      </Link>
+    </Snav>
   );
 };
 
