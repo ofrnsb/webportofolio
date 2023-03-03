6d0da3f89f9b2e434724a113cedd25f51355323d
b <ofbutar@gmail.com>
2023-03-04 00:31:25 +0700

    add remote URL

--- src/Modals/Url/Url.js
+++ src/Modals/Url/Url.js
@@ -1,5 +1,5 @@
 // export const baseUrl = 'https://127.0.0.1:8080';
-export const baseUrl = 'http://localhost:8080';
+export const baseUrl = 'https://portfoliobe.cleverapps.io';
 
 // server.ssl.key-store-type=PKCS12
 // server.ssl.key-store=/Users/b/2023/pp/pb/localhost.p12
