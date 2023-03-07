a55ca40db18479f8a395e2c19c782bf705886506
b <ofbutar@gmail.com>
2023-03-07 10:40:40 +0700

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,47 +1,14 @@
-// // `next-pwa` config should be passed here
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-//   register: true,
-//   disable: process.env.NODE_ENV === 'development',
-// });
+// `next-pwa` config should be passed here
 
-// const nextConfig = withPWA({
-//   reactStrictMode: true,
-//   // webpack5: true,
-//   webpack: (config) => {
-//     config.resolve.fallback = { fs: false };
-//     return config;
-//   },
-//   compiler: {
-//     styledComponents: true,
-//   },
-//   images: {
-//     domains: [
-//       'pbs.twimg.com',
-//       'img.icons8.com',
-//       'gateway.moralisipfs.com',
-//       'ipfs.moralis.io',
-//       'lh3.googleusercontent.com',
-//       'www.artnews.com',
-//     ],
-//   },
-//   output: 'standalone',
-// });
-
-// // Use `withPWA` and pass general Next.js config
-// module.exports = nextConfig;
-
-/////BATAS
-
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
   reactStrictMode: true,
+  // webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
