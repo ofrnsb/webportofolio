dc770dfd277c0843b53eaf2e45049a0c061ee90d
b <ofbutar@gmail.com>
2023-03-07 11:19:26 +0700

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 // `next-pwa` config should be passed here
+const runtimeCaching = require('next-pwa/cache');
 
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
-  disable: process.env.NODE_ENV === 'development',
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
 });
 
 const nextConfig = withPWA({
