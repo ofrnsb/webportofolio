b99e04b1bda9e5f54063b1ae4afeb56e19b1ea6d
b <ofbutar@gmail.com>
2023-03-07 09:51:58 +0700

    move to PWA, add .env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,32 @@
-/** @type {import('next').NextConfig} */
+// `next-pwa` config should be passed here
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
-const nextConfig = {
-  reactStrictMode: false,
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  // webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
   compiler: {
     styledComponents: true,
   },
-};
+  images: {
+    domains: [
+      'pbs.twimg.com',
+      'img.icons8.com',
+      'gateway.moralisipfs.com',
+      'ipfs.moralis.io',
+      'lh3.googleusercontent.com',
+      'www.artnews.com',
+    ],
+  },
+  output: 'standalone',
+});
 
+// Use `withPWA` and pass general Next.js config
 module.exports = nextConfig;
--- src/Modals/Url/Url.js
+++ src/Modals/Url/Url.js
@@ -1,5 +1,5 @@
 // export const baseUrl = 'https://127.0.0.1:8080';
-export const baseUrl = 'https://portfoliobe.cleverapps.io';
+export const baseUrl = process.env.NEXT_PUBLIC_baseUrl;
 
 // server.ssl.key-store-type=PKCS12
 // server.ssl.key-store=/Users/b/2023/pp/pb/localhost.p12
